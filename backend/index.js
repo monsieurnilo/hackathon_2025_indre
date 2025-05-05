b4c3b0ae1084d640a5061e466f8cfd16b3e47c84
Louis Tamagny <louis.tamagny@gmail.com>
2025-05-05 16:26:56 +0200

    feat: add swagger view at /api endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const swaggerView = require('./swagger');
 const app = express();
 const port = 3000;
 
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     description: Get the server status
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: The server status
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   description: The server status
+ */
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.use("/api", swaggerView);
 
 app.listen(port, () => {
     console.log(`Serveur démarré sur http://localhost:${port}`);
