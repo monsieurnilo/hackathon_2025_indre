895ec9790fcfeac70578ef72e58342a17d30ca70
Raphaël P. <raphaelpavard@gmail.com>
2025-05-05 16:47:26 +0200

    feat: ajouter le modèle et le service pour les municipalités

---
+++ backend/src/models/municipality.js
@@ -0,0 +1,64 @@
+// File: commune.js
+// Description: Model representing a French commune/municipality
+
+class Municipality {
+    /**
+     * Create a Commune instance
+     * @param {Object} data - Commune data
+     * @param {string} data.nom - Name of the commune
+     * @param {string} data.code - Commune code (INSEE code)
+     * @param {string} data.codeDepartement - Department code
+     * @param {string} data.siren - SIREN identifier
+     * @param {string} data.codeEpci - EPCI code (Établissement public de coopération intercommunale)
+     * @param {string} data.codeRegion - Region code
+     * @param {string[]} data.codesPostaux - Array of postal codes
+     * @param {number} data.population - Population count
+     */
+    constructor({
+        nom,
+        code,
+        codeDepartement,
+        siren,
+        codeEpci,
+        codeRegion,
+        codesPostaux,
+        population
+    }) {
+        this.nom = nom;
+        this.code = code;
+        this.codeDepartement = codeDepartement;
+        this.siren = siren;
+        this.codeEpci = codeEpci;
+        this.codeRegion = codeRegion;
+        this.codesPostaux = codesPostaux;
+        this.population = population;
+    }
+
+    /**
+     * Create a Commune from JSON data
+     * @param {Object} jsonData - JSON representation of a commune
+     * @returns {Municipality} - New Commune instance
+     */
+    static fromJson(jsonData) {
+        return new Municipality(jsonData);
+    }
+
+    /**
+     * Convert Commune to JSON representation
+     * @returns {Object} - JSON representation
+     */
+    toJson() {
+        return {
+            nom: this.nom,
+            code: this.code,
+            codeDepartement: this.codeDepartement,
+            siren: this.siren,
+            codeEpci: this.codeEpci,
+            codeRegion: this.codeRegion,
+            codesPostaux: this.codesPostaux,
+            population: this.population
+        };
+    }
+}
+
+module.exports = Municipality;
